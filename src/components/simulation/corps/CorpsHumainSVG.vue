15b4a46947e6e6da6c85c18b06cb3fe99332c358
achrafElFaiq <90605410+achrafElFaiq@users.noreply.github.com>
2024-12-05 22:57:39 +0100

    Agrandissement image

--- src/components/simulation/corps/CorpsHumainSVG.vue
+++ src/components/simulation/corps/CorpsHumainSVG.vue
@@ -38,16 +38,17 @@ const getOrganeClass = computed(() => (organeId: string) => {
 </script>
 
 <template>
-  <svg class="w-full h-full" viewBox="0 0 400 600">
+  <svg class="w-full h-full" viewBox="0 0 400 600" >
     <!-- Silhouette du corps -->
-    <image 
-      x="-70" 
-      y="-50" 
-      width="510" 
-      height="700" 
-      href="../../../assets/corps.svg" 
-      class="opacity-70"
-    />
+    <image
+    x="0"
+    y="0"
+    width="100%" 
+    height="100%" 
+    href="../../../assets/corps.svg" 
+    class="opacity-70"
+    preserveAspectRatio="xMidYMid meet"
+  />
     
     <!-- Organes -->
     <template v-for="organe in organes" :key="organe.id">
