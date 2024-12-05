fda80e4a4020b5380a708dccbc4f8d6988dc0577
Clément <clement.lacroix@insa-rouen.fr>
2024-12-05 21:52:10 +0100

    fin de la section a propos de nous

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,38 +8,51 @@ const equipeInfo = ref([
     nom: 'Hugo Tondenier',
     role: 'Directeur de Projet ',
     description: 'Passionné par la protection des océans depuis 22 ans',
-    image: '/images/team/hugo.jpg'
+    image: '/images/team/hugo.jpg',
+    lien : 'https://hough.tondenier.fr/'
   },
   {
     nom: 'Julien Vollet',
     role: 'Responsable Technique',
     description: 'Expert en solutions durables pour l\'environnement marin',
-    image: '/images/team/julien.jpg'
+    image: '/images/team/julien.jpg',
+    lien : 'https://www.instagram.com/ju_vllt_/'
   },
   {
     nom: 'Ziyad Gnaoui',
     role: 'Coordinateur Scientifique',
     description: 'Spécialiste en écosystèmes marins',
-    image: '/images/team/ziyad.jpeg'
+    image: '/images/team/ziyad.jpeg',
+    lien : 'https://www.linkedin.com/in/ziyad-gnaoui-8b1453211/'
   },
   {
     nom: 'Elouen Lefeuvre',
     role: 'Responsable Communication',
     description: 'Expert en sensibilisation environnementale',
-    image: '/images/team/elouen.jpg'
+    image: '/images/team/elouen.jpg',
+    lien : 'https://www.linkedin.com/in/elouen-lefeuvre-52038b239/'
   },
   {
     nom: 'Achraf El Faiq',
     role: 'Biologiste Marine',
     description: 'Spécialiste de la biodiversité océanique',
-    image: '/images/team/achraf.jpg'
+    image: '/images/team/achraf.jpg',
+    lien : 'https://www.linkedin.com/in/achraf-el-faiq-789323206/'
   },
   {
     nom: 'Clément Lacroix',
     role: 'Ingénieur Environnemental',
     description: 'Expert en solutions de dépollution marine',
-    image: '/images/team/clement.jpg'
+    image: '/images/team/clement.jpg',
+    lien : 'https://www.linkedin.com/in/cl%C3%A9ment-lacroix18/ '
   },
+  {
+    nom : 'Chat GPT',
+    role : 'Assistant Developpement',
+    description : 'Spécialiste des codes non fonctionnels',
+    image : '/images/team/gpt.jpg',
+    lien : 'https://chatgpt.com/'
+  }
 ])
 
 const missionTexte = ref(`Notre mission est de sensibiliser et d'agir pour la protection des océans. 
@@ -59,13 +72,13 @@ Nous travaillons sans relâche pour préserver la biodiversité marine et lutter
       </div>
 
       <h2 class="text-3xl text-white font-semibold mb-8 text-center">Notre Équipe</h2>
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center place-items-center">
-      <MembreEquipe
-        v-for="membre in equipeInfo"
-        :key="membre.nom"
-        v-bind="membre"
-      />
-    </div>
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-center">
+        <MembreEquipe
+          v-for="membre in equipeInfo"
+          :key="membre.nom"
+          v-bind="membre"
+        />
+      </div>
     </div>
 
     <VaguesDivider position="bas" />
